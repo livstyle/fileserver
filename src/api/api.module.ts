755ecc574597f229ec64d1333f32de10a51b2250
jiangkun <jiang123986@163.com>
2021-09-13 14:14:54 +0800

    excel

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -20,21 +20,22 @@ export class Api {
         const worksheet = workbook.addWorksheet('数据流', { views: [{ state: 'frozen', xSplit: 1, ySplit: 3 }] });
 
         let filename = "数据流表";
-        res.setHeader('Connection', 'keep-alive');
-        res.setHeader('Content-Type', 'application/octet-stream');
+        res.setHeader('Connection', 'keep-alive'); // 保持链接一直在
+        res.setHeader('Content-Type', 'application/octet-stream'); // 文件类型为文件流形式
         res.setHeader("Content-Disposition", "attachment; filename=" + encodeURIComponent(filename) + ".xlsx");
+        res.flushHeaders(); // 先将headers返回
 
         // @ts-ignore
-        workbook.stream.pipe(res);
+        workbook.stream.pipe(res); // 使用管道流将xlsx流数据直接写入response流中
 
         for (let index = 0; index < 500000; index++) {
             const row = [];
             for (let i = 0; i < 20; i++) {
                 row.push(i);
             }
-            worksheet.addRow(row).commit();
+            worksheet.addRow(row).commit(); // commit() 提交流数据
         }
-        await workbook.commit();
+        workbook.commit(); // 提交整个Excel工作簿
 
     }
 
